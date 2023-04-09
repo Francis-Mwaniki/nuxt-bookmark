00f6699452ac4f6c758fa836c77af7f096595583
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-04-09 22:31:01 +0300

    setup for bookmark

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito&family=Space+Mono&display=swap");
+
+* {
+  font-family: "Space Mono", monospace;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules:[ '@nuxt/devtools','nuxt-icon','@nuxtjs/tailwindcss','@nuxt/image-edge'],
+  devtools: {
+    // Enable devtools (default: true)
+    enabled: true,
+    // VS Code Server options
+    vscode: {},
+    // ...other options
+  },
+  css: ['~/assets/css/main.css'],
+  buildModules:[
+    '@nuxt/image',
+  ]
+
+})
