2f2594d9b9e3a183cc665f888ae80722421692e7
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-04-10 02:16:39 +0300

    NEW update

--- app.vue
+++ app.vue
@@ -1,12 +1,85 @@
 <template>
   <div>
-    <NuxtPage />
+    <div class="  ">
+      <NuxtPage />
+    </div>
+
+    <!-- developed by francis -->
+    <footer
+      class="bg-gradient-to-r from-black via-[#01397A] border-none to-[#01397A] py-4 relative"
+    >
+      <div
+        class="container mx-auto text-white text-center nav-font flex flex-row justify-around items-center"
+      >
+        <p class="nav-font">&copy; Francis 2023 &trade; &copy; Copyrights</p>
+        <div class="flex justify-center items-center mx-auto gap-x-2 py-1">
+          <a
+            href=""
+            class="text-white list-none no-underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon name="uil:github" class="h-7 w-7" />
+          </a>
+          <a
+            href=""
+            class="text-white list-none no-underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon name="uil:twitter" class="h-7 w-7" />
+          </a>
+          <a
+            href=""
+            class="text-white list-none no-underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon name="uil:linkedin" class="h-7 w-7" />
+          </a>
+        </div>
+      </div>
+    </footer>
   </div>
+
+  <svg
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1200 120"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+      class="shape-fill"
+      fill="#01397A"
+      fill-opacity="1"
+    ></path>
+  </svg>
 </template>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap");
 
 * {
   font-family: "Quicksand", sans-serif;
+}
+.custom-shape-divider-top-1681081177 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+}
+
+.custom-shape-divider-top-1681081177 svg {
+  position: relative;
+  display: block;
+  width: calc(109% + 1.3px);
+  height: 396px;
+  transform: rotateY(180deg);
+}
+
+.custom-shape-divider-top-1681081177 .shape-fill {
+  fill: #01397a;
 }
 </style>
--- components/CreateBookMark.vue
+++ components/CreateBookMark.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="min-h-screen bg-gradient-to-tr from-black via-indigo-800 to-black">
-    <div class="flex flex-col items-center bg-white max-w-lg rounded py-5 mx-auto px-2">
-      <h1 class="text-2xl font-bold mb-4">Create Bookmark</h1>
+  <div class="min-h-screen bg-gradient-to-tr from-black via-[#01397A] to-black">
+    <div
+      class="flex flex-col items-center max-w-lg rounded py-5 mx-auto px-2 bg-gradient-to-tr from-[#01397A] via-[#01397A] to-[#01397A]"
+    >
+      <h1 class="text-2xl font-bold text-white mb-4">Create Bookmark</h1>
       <form class="w-full max-w-lg" @submit.prevent="createBookmark">
         <div class="flex flex-wrap mb-6">
           <div class="w-full mb-3">
-            <label class="block text-gray-700 font-bold mb-2" for="title"> Title </label>
+            <label class="block text-gray-200 font-bold mb-2" for="title"> Title </label>
             <input
               v-model="newBookmark.title"
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -16,7 +18,7 @@
             />
           </div>
           <div class="w-full mb-3">
-            <label class="block text-gray-700 font-bold mb-2" for="url"> URL </label>
+            <label class="block text-gray-200 font-bold mb-2" for="url"> URL </label>
             <input
               v-model="newBookmark.url"
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -27,7 +29,7 @@
             />
           </div>
           <div class="w-full mb-3">
-            <label class="block text-gray-700 font-bold mb-2" for="description">
+            <label class="block text-gray-200 font-bold mb-2" for="description">
               Description
             </label>
             <textarea
@@ -39,7 +41,7 @@
             ></textarea>
           </div>
           <div class="w-full mb-3">
-            <label class="block text-gray-700 font-bold mb-2" for="icon"> Icon </label>
+            <label class="block text-gray-200 font-bold mb-2" for="icon"> Icon </label>
             <input
               v-model="newBookmark.icon"
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -51,14 +53,24 @@
           </div>
         </div>
         <div class="flex justify-end">
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
+          <nuxt-link
+            to="NewBookMark"
+            class="bg-yellow-600 text-white px-4 py-2 rounded-lg cursor-pointer ring-1 ring-white my-2"
           >
-            Create
-          </button>
+            Create Bookmark
+            <Icon name="ic:round-bookmark-add" class="h-10 w-10" />
+          </nuxt-link>
         </div>
       </form>
+    </div>
+    <div class="flex justify-center">
+      <nuxt-link
+        to="/"
+        class="bg-yellow-600 text-white px-4 py-2 rounded-lg cursor-pointer ring-1 ring-white my-2"
+      >
+        Back to Home
+        <Icon name="ic:round-home" class="h-10 w-10" />
+      </nuxt-link>
     </div>
   </div>
 </template>
