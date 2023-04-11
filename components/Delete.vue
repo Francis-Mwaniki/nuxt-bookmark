14c4eb5ab971f57f780cfe07b2f74b96f76d84de
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-04-11 23:57:01 +0300

    more releases

--- components/Delete.vue
+++ components/Delete.vue
@@ -9,7 +9,7 @@
       <p class="mb-4">Are you sure you want to delete this bookmark?</p>
       <div class="flex justify-end">
         <button
-          class="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 mr-2"
+          class="px-4 py-2 bg-yellow-700 text-white rounded-md hover:bg-yellow-800 mr-2"
           @click="closeModal"
         >
           Cancel
