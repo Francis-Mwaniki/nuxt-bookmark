b6fcded579474071a8fc20c85f4920176abde7d8
Francis Mwaniki <105497998+Francis-Mwaniki@users.noreply.github.com>
2023-04-16 16:44:39 +0300

    Update our-story.vue

--- pages/our-story.vue
+++ pages/our-story.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto mt-8">
+  <div class="container md:mx-auto mx-3 mt-8">
     <h1 class="text-3xl font-bold mb-4">Our Story</h1>
     <p class="text-gray-700 leading-relaxed">
       We started our company with a simple goal in mind - to help people organize their
